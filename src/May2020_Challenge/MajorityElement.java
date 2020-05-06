14318d608f8cd0716702de13b5604fe0d4f2d71d
adilkhan <adilkhan@localhost>
2020-05-07 01:46:43 +0530

    updated code with moore voting algo

--- src/May2020_Challenge/MajorityElement.java
+++ src/May2020_Challenge/MajorityElement.java
@@ -3,6 +3,8 @@ package May2020_Challenge;
 import java.util.HashMap;
 
 public class MajorityElement {
+	
+	//O(n) time but 2 passes and O(n) space
 
 	public static int majorityElement(int[] nums) {
 
@@ -24,6 +26,38 @@ public class MajorityElement {
 		return -1; // for java sake
 
 	}
+	
+	//O(n) time single pass and O(1) space
+	
+	//Boyer - Moore Majority Voting Algorithm
+	
+	public static int majorityElement_Best_Approach(int arr[])
+	{
+		
+		int majority = arr[0];
+		
+		int count = 1;
+		
+		for (int i = 1; i < arr.length; i++)
+		{
+		   if (arr[i] == majority)
+		   {
+			   count+=1;
+		   }else
+		   {
+			   count-=1;
+			   if(count == 0)
+			   {
+				   majority=arr[i];
+				   count = 1;
+			   }
+		   }
+			
+		}
+		
+		
+		return majority;
+	}
 
 	public static void main(String[] args) {
 
@@ -31,7 +65,9 @@ public class MajorityElement {
 
 		int arr1[] = { 2, 2, 1, 1, 1, 2, 2 }; // 2
 
-		System.out.println(majorityElement(arr1));
+		//System.out.println(majorityElement(arr1));
+		
+		System.out.println(majorityElement_Best_Approach(arr1));
 
 	}
 
