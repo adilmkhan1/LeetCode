5cbbe4b4f38eecbaf74bd13befed5cb5f7b4869f
adilkhan <adilkhan@192.168.0.118>
2020-05-10 16:44:27 +0530

    Update code with best approach

--- src/May2020_Challenge/ValidPerfectSquare.java
+++ src/May2020_Challenge/ValidPerfectSquare.java
@@ -1,38 +1,66 @@
 package May2020_Challenge;
 
 public class ValidPerfectSquare {
-	
-	
-	public static boolean isPerfectSquare(int num)
-	{
-		
+
+	public static boolean isPerfectSquare(int num) {
+
 		if (num < 2)
 			return true;
-		
-		for (int i = 2; i <= num/2; i++) 
-		{
-			int curr = i*i;
-			
-			if (curr == num)
-			{
+
+		for (int i = 2; i <= num / 2; i++) {
+			int curr = i * i;
+
+			if (curr == num) {
 				return true;
 			}
 		}
-		
-		
-		
+
 		return false;
 	}
 
+	// time complexity - O(log(sqrt(n))
+
+	public static boolean isPerfectSquare_Efficient(int num) {
+
+		if (num < 2)
+			return true;
+
+		int low = 2;
+
+		int high = num / 2;
+
+		int mid;
+
+		int sq;
+
+		while (low <= high) {
+			mid = low + (high - low) / 2;
+
+			sq = mid * mid;
+
+			if (sq == num)
+				return true;
+
+			else if (sq > num)
+				high = mid - 1;
+			else
+				low = mid + 1;
+
+		}
+
+		return false;
+
+	}
+
 	public static void main(String[] args) {
-		
-       int num = 16; //true
-       
-       int num1 = 14; //false
-       
-       System.out.println(isPerfectSquare(4));
-       
-		
+
+		int num = 16; // true
+
+		int num1 = 14; // false
+
+		// System.out.println(isPerfectSquare(4));
+
+		System.out.println(isPerfectSquare_Efficient(25));
 
 	}
 
